6dab39fd1bf4d5f40bf52cdafec5598289ec3852
stockizenresearch-sudo <stockizenresearch@gmail.com>
2025-12-30 19:08:06 +0530

    make the header design proper.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Sora } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/Footer";
+import SolfitHeader from "./components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${sora.variable} antialiased`}
       >
+        <SolfitHeader />
         {children}
         <Footer />
       </body>
