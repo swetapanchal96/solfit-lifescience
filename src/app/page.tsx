342f856af2a80cc8d3f4fe5e8b8aa44ea9b761ec
stockizenresearch-sudo <stockizenresearch@gmail.com>
2025-12-31 18:53:06 +0530

    working in the home page design converting.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+import ClientLogo from "./components/ClientLogo";
 import SolfitHeader from "./components/Header";
 import Hero from "./components/Hero";
+import HomeAbout from "./components/HomeAbout";
+import WhyChooseUs from "./components/HomeWhyChooseUs";
 
 export default function Home() {
   return (
     <div className="">
       <Hero />
+      <HomeAbout />
+      <WhyChooseUs />
+      <ClientLogo />
     </div>
     // <SolfitHeader />
   );
